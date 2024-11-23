1c6e890bc2e4e125af1fa7b5a7f21b80da5e24d7
Thanu Poptiphueng <tpcrasite@gmail.com>
2024-11-23 11:32:13 +0700

    reformat

--- cherrypy/main.py
+++ cherrypy/main.py
@@ -4,17 +4,17 @@ import shlex
 # Get the list of all files and directories
 
 
-
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
         return "Site Under Construction<br/>use /generate?cmd=&lt;YOUR COMMAND&gt;<br/>to execute command to configure the server as ssh is not installed.<br /> <br />"
 
     @cherrypy.expose
-    def generate(self,cmd):
+    def generate(self, cmd):
         proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
         output = proc.stdout.read()
         return output
+
 
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.quickstart(HelloWorld())
